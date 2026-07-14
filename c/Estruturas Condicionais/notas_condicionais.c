d514b5038efe7a21e4e019bbdbd51ecb5d1a2182
SandyTorresTavares <sandyt.tavares@gmail.com>
2026-07-14 20:03:59 -0300

    adiciona exercicios

--- c/Estruturas Condicionais/notas_condicionais.c
+++ c/Estruturas Condicionais/notas_condicionais.c
@@ -20,14 +20,15 @@ int main() {
  	media = (nota1 + nota2) / 2;
 
  	if (media >= 7.0) {
-    printf("Parabens! Voce foi aprovado com a nota %lf\n", media);
+    printf("Parabens! Voce foi aprovado com a nota %.2lf\n", media);
 }
 else if (media >= 5.0) {
-    printf("Voce esta em Recuperacao\nSua media e: %lf\n", media);
+    printf("Voce esta em Recuperacao\nSua media e: %.2lf\n", media);
 }
 else {
-    printf("Reprovado!\n Sua media e %lf\n", media);
+    printf("Reprovado!\n Sua media e %.2lf\n", media);
 } 
+return 0;
 }
 
 
