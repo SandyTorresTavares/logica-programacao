ccee372c12f8dfffa940829938f54c8c617e06d0
SandyTorresTavares <sandyt.tavares@gmail.com>
2026-07-15 11:28:57 -0300

    ajustes

--- c/Estruturas repetitivas/senha.c
+++ c/Estruturas repetitivas/senha.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
 int main() {
-	int senhaCorreta, senha;
 
-	senhaCorreta = 2002;
+    int senhaCorreta = 2002;
+    int senha;
 
-	printf("Digite sua senha:\n");
-	scanf("%d", &senha);
+    printf("Digite sua senha:\n");
+    scanf("%d", &senha);
 
-	while(senha != senhaCorreta) {
-		printf("Senha incorreta\n");
-		printf("Digite sua senha:\n");
-		scanf("%d", &senha);
-	}
+    while (senha != senhaCorreta) {
+        printf("Senha incorreta\n");
+        printf("Digite sua senha:\n");
+        scanf("%d", &senha);
+    }
 
-	if (senha == senhaCorreta);
-	{
-		printf("Acesso liberado");
-	}
-	
-		return 0;
+    printf("Acesso liberado\n");
 
+    return 0;
 }
